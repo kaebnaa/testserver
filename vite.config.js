dff00de117cb48380f96b30f86554cbf5af64d7f
123 <suhbat8575@gmail.com>
2025-05-26 14:48:16 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // Dev server дээр SPA fallback
+    historyApiFallback: true,
+  },
+});
